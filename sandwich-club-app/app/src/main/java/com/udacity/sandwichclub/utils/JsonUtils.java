307ed55479edb12e27d547c21d3eb97f4c63fc49
Daniel Knapp <opendroid@gmail.com>
2018-04-29 15:31:14 -0400

    parsing json data into sandwich object

--- sandwich-club-app/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club-app/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,60 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
+    private static final String TAG = "JsonUtils";
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        //parse details
+        JSONObject jsonObject;
+        JSONObject nameJsonObject;
+        Sandwich sandwich = new Sandwich();
+
+        try {
+            jsonObject = new JSONObject(json);
+
+            // name has sub level
+            nameJsonObject = jsonObject.getJSONObject("name");
+            sandwich.setMainName(nameJsonObject.getString("mainName"));
+            sandwich.setAlsoKnownAs(getListFromArray(nameJsonObject.getJSONArray("alsoKnownAs")));
+
+            // these are all on main level
+            sandwich.setDescription(jsonObject.getString("description"));
+            sandwich.setImage(jsonObject.getString("image"));
+            sandwich.setIngredients(getListFromArray(jsonObject.getJSONArray("ingredients")));
+            sandwich.setPlaceOfOrigin(jsonObject.getString("placeOfOrigin"));
+
+        } catch (JSONException e) {
+            Log.d(TAG, "parseSandwichJson: Exception " + e.toString());
+            e.printStackTrace();
+        }
+
+        return sandwich;
+    }
+
+    private static List<String> getListFromArray(JSONArray array) {
+
+        List<String> list = new ArrayList<>();
+
+        try {
+            for (int i = 0; i < array.length(); i++) {
+                list.add(array.getString(i));
+            }
+        } catch (JSONException e) {
+            Log.d(TAG, "getListFromArray: Exception " + e.toString());
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
